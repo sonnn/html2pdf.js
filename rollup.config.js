e3ccb07fead0d70a4f54184fc00b8829b18b5d11
dev <dev@gic.com>
2019-08-28 17:23:12 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,73 +1,66 @@
 // Import dependencies.
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
-import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import replace from "rollup-plugin-replace";
+import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
+import pkg from "./package.json";
 
 function banner() {
-  var text = pkg.name + ' v' + pkg.version + '\n';
-  text += 'Copyright (c) ' + (new Date).getFullYear() + ' Erik Koopmans\n';
-  text += 'Released under the ' + pkg.license + ' License.';
-  text = '/**\n * ' + text.replace(/\n/g, '\n * ') + '\n */';
+  var text = pkg.name + " v" + pkg.version + "\n";
+  text += "Copyright (c) " + new Date().getFullYear() + " Erik Koopmans\n";
+  text += "Released under the " + pkg.license + " License.";
+  text = "/**\n * " + text.replace(/\n/g, "\n * ") + "\n */";
   return { banner: text };
 }
 function license(filename) {
-  filename = filename || './LICENSE';
+  filename = filename || "./LICENSE";
   var data = fs.readFileSync(filename).toString();
-  data = '/**\n * @license\n * ' + data.trim().replace(/\n/g, '\n * ') + '\n */\n';
+  data =
+    "/**\n * @license\n * " + data.trim().replace(/\n/g, "\n * ") + "\n */\n";
   return { banner: data };
 }
 
 export default [
   // Un-bundled builds.
   {
-    name: 'html2pdf',
-    input: 'src/index.js',
+    name: "html2pdf",
+    input: "src/index.js",
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-      { file: pkg.browser, format: 'umd' }
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+      { file: pkg.browser, format: "umd" }
     ],
-    external: [
-      'jspdf',
-      'html2canvas',
-    ],
+    external: ["jspdf-fix-html2pdf", "html2canvas"],
     globals: {
-      'es6-promise': 'es6promise',
-      jspdf: 'jsPDF',
-      html2canvas: 'html2canvas'
+      "es6-promise": "es6promise",
+      "jspdf-fix-html2pdf": "jsPDF",
+      html2canvas: "html2canvas"
     },
     plugins: [
       resolve(),
       commonjs(),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      babel({ exclude: 'node_modules/**' }),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      babel({ exclude: "node_modules/**" }),
       banner()
     ]
   },
   // Un-bundled builds (minified).
   {
-    name: 'html2pdf',
-    input: 'src/index.js',
-    output: [
-      { file: pkg.browser.replace(/js$/, 'min.js'), format: 'umd' }
-    ],
-    external: [
-      'jspdf',
-      'html2canvas',
-    ],
+    name: "html2pdf",
+    input: "src/index.js",
+    output: [{ file: pkg.browser.replace(/js$/, "min.js"), format: "umd" }],
+    external: ["jspdf-fix-html2pdf", "html2canvas"],
     globals: {
-      'es6-promise': 'es6promise',
-      jspdf: 'jsPDF',
-      html2canvas: 'html2canvas'
+      "es6-promise": "es6promise",
+      "jspdf-fix-html2pdf": "jsPDF",
+      html2canvas: "html2canvas"
     },
     plugins: [
       resolve(),
       commonjs(),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      babel({ exclude: 'node_modules/**' }),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      babel({ exclude: "node_modules/**" }),
       uglify({
         output: { preamble: banner().banner }
       })
@@ -75,41 +68,39 @@ export default [
   },
   // Bundled builds.
   {
-    name: 'html2pdf',
-    input: 'src/index.js',
-    output: [
-      { file: pkg.browser.replace(/js$/, 'bundle.js'), format: 'umd' }
-    ],
+    name: "html2pdf",
+    input: "src/index.js",
+    output: [{ file: pkg.browser.replace(/js$/, "bundle.js"), format: "umd" }],
     globals: {
-      'es6-promise': 'es6promise',
-      jspdf: 'jsPDF',
-      html2canvas: 'html2canvas'
+      "es6-promise": "es6promise",
+      "jspdf-fix-html2pdf": "jsPDF",
+      html2canvas: "html2canvas"
     },
     plugins: [
       resolve(),
       commonjs(),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      babel({ exclude: 'node_modules/**' }),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      babel({ exclude: "node_modules/**" }),
       banner()
     ]
   },
   // Bundled builds (minified).
   {
-    name: 'html2pdf',
-    input: 'src/index.js',
+    name: "html2pdf",
+    input: "src/index.js",
     output: [
-      { file: pkg.browser.replace(/js$/, 'bundle.min.js'), format: 'umd' }
+      { file: pkg.browser.replace(/js$/, "bundle.min.js"), format: "umd" }
     ],
     globals: {
-      'es6-promise': 'es6promise',
-      jspdf: 'jsPDF',
-      html2canvas: 'html2canvas'
+      "es6-promise": "es6promise",
+      "jspdf-fix-html2pdf": "jsPDF",
+      html2canvas: "html2canvas"
     },
     plugins: [
       resolve(),
       commonjs(),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      babel({ exclude: 'node_modules/**' }),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      babel({ exclude: "node_modules/**" }),
       uglify({
         output: { preamble: banner().banner }
       })
